2bf67b6cdfb03832e75b9d6ade34d410bf50c10c
Alexandr <91020990+Allleksandr4444@users.noreply.github.com>
2022-02-25 15:05:53 +0300

    Решение заданий 3 дня

--- tasks/magic_method/task_1/implementation.py
+++ tasks/magic_method/task_1/implementation.py
@@ -1,24 +1,92 @@
 class Multiplier:
-    def __init__(self):
-        pass
+    def __init__(self, value):
+        self.__number = value
+
+    def get_value(self):
+        return int(self.__number)
+
+    def __add__(self, other):
+        if isinstance(other, Multiplier):
+            value = self.__number + other.__number
+        else:
+            value = self.__number
+
+        return Multiplier(value)
+
+    def __radd__(self, other):
+        if isinstance(other, Multiplier):
+            value = other.__number + self.__number
+        else:
+            value = self.__number
+
+        return value
+
+    def __sub__(self, other):
+        if isinstance(other, Multiplier):
+            value = self.__number - other.__number
+        else:
+            value = self.__number
+
+        return Multiplier(value)
+
+    def __rsub__(self, other):
+        if isinstance(other, Multiplier):
+            value = other.__number - self.__number
+        else:
+            value = self.__number
+
+        return value
+
+    def __mul__(self, other):
+        if isinstance(other, Multiplier):
+            value = self.__number * other.__number
+        else:
+            value = self.__number
+
+        return Multiplier(value)
+
+    def __rmul__(self, other):
+        if isinstance(other, Multiplier):
+            value = other.__number * self.__number
+        else:
+            value = self.__number
+
+        return value
+
+    def __truediv__(self, other):
+        if isinstance(other, Multiplier):
+            value = self.__number / other.__number
+        else:
+            value = self.__number
+
+        return Multiplier(value)
+
+    def __rtruediv__(self, other):
+        if isinstance(other, Multiplier):
+            value = other.__number / self.__number
+        else:
+            value = self.__number
+
+        return value
 
 
 class Hundred(Multiplier):
     """Множитель на 100"""
-    def __init__(self):
-        pass
+
+    def __init__(self, value):
+        super().__init__(value * 100)
 
 
 class Thousand(Multiplier):
     """Множитель на 1 000"""
-    def __init__(self):
-        pass
+
+    def __init__(self, value):
+        super().__init__(value * 1000)
 
 
 class Million(Multiplier):
     """Множитель на 1 000 000"""
-    def __init__(self):
-        pass
 
-
+    def __init__(self, value):
+        super().__init__(value * 1000000)
 
--- tasks/magic_method/task_2/implementation.py
+++ tasks/magic_method/task_2/implementation.py
@@ -1,8 +1,35 @@
+import datetime
+
+
 class MathClock:
     def __init__(self):
-        pass
+        date_obj = datetime.datetime(2022, 2, 25)
+        self.__time_now = datetime.datetime(year=date_obj.year, month=date_obj.month, day=date_obj.day)
 
     def get_time(self):
-        return 0
+        return self.__time_now.strftime('%H:%M')
 
+    def __add__(self, other):
+        if type(other) != int:
+            raise 'Введите целое число!'
+        time = datetime.timedelta(minutes=int(other))
+        self.__time_now += time
+
+    def __sub__(self, other):
+        if type(other) != int:
+            raise 'Введите целое число!'
+        time = datetime.timedelta(minutes=int(other))
+        self.__time_now -= time
+
+    def __mul__(self, other):
+        if type(other) != int:
+            raise 'Введите целое число!'
+        time = datetime.timedelta(hours=int(other))
+        self.__time_now += time
+
+    def __truediv__(self, other):
+        if type(other) != int:
+            raise 'Введите целое число!'
+        time = datetime.timedelta(hours=int(other))
+        self.__time_now -= time
 
